1fc84673dd73485a4beffe2ec204e1457e0669da
Youssef-Darrag <darrag904@gmail.com>
2025-11-19 23:39:25 +0200

    Add constant data.

---
+++ ECommerce.Core/Constants/DataIDs.cs
@@ -0,0 +1,140 @@
+﻿namespace ECommerce.Core.Constants;
+
+public static class CategoryIds
+{
+    // Main Category
+    public const string TraditionalEgyptianFood = "01HQ5RZJXP1MAWB3DQXS5QNZEH";
+
+    // Sub Categories
+    public const string GrilledFoods = "01HQ5RZJXP2MAWB3DQXS5QNZEI";
+    public const string OrientalSweets = "01HQ5RZJXP3MAWB3DQXS5QNZEJ";
+    public const string Bakery = "01HQ5RZJXP4MAWB3DQXS5QNZEK";
+    public const string Seafood = "01HQ5RZJXP5MAWB3DQXS5QNZEL";
+    public const string Breakfast = "01HQ5RZJXP6MAWB3DQXS5QNZEM";
+    public const string Appetizers = "01HQ5RZJXP7MAWB3DQXS5QNZEN";
+    public const string Soups = "01HQ5RZJXP8MAWB3DQXS5QNZEO";
+    public const string RiceDishes = "01HQ5RZJXP9MAWB3DQXS5QNZEP";
+    public const string PastaDishes = "01HQ5RZJXPAMAWB3DQXS5QNZEQ";
+    public const string VegetablesAndLegumes = "01HQ5RZJXPBMAWB3DQXS5QNZER";
+    public const string HotBeverages = "01HQ5RZJXPCMAWB3DQXS5QNZES";
+    public const string NaturalJuices = "01HQ5RZJXPDMAWB3DQXS5QNZET";
+    public const string ChickenDishes = "01HQ5RZJXPEMAWB3DQXS5QNZEU";
+    public const string MeatDishes = "01HQ5RZJXPFMAWB3DQXS5QNZEV";
+    public const string VegetarianDishes = "01HQ5RZJXPGMAWB3DQXS5QNZEW";
+    public const string Pickles = "01HQ5RZJXPHMAWB3DQXS5QNZEX";
+    public const string SpicesAndSeasonings = "01HQ5RZJXPIMAWB3DQXS5QNZEY";
+    public const string SaucesAndDips = "01HQ5RZJXPJMAWB3DQXS5QNZEZ";
+    public const string FastFood = "01HQ5RZJXPKMAWB3DQXS5QNZF0";
+}
+
+public static class ProductIds
+{
+    public const string KosharyClassic = "01HQ6BZJXP1MAWB3DQXS5QNZE1";
+    public const string MolokhiaWithChicken = "01HQ6BZJXP2MAWB3DQXS5QNZE2";
+    public const string FulMedames = "01HQ6BZJXP3MAWB3DQXS5QNZE3";
+    public const string MixedGrill = "01HQ6BZJXP4MAWB3DQXS5QNZE4";
+    public const string Kunafa = "01HQ6BZJXP5MAWB3DQXS5QNZE5";
+    public const string BaladiBread = "01HQ6BZJXP6MAWB3DQXS5QNZE6";
+    public const string GrilledSeabass = "01HQ6BZJXP7MAWB3DQXS5QNZE7";
+    public const string FattahWithMeat = "01HQ6BZJXP8MAWB3DQXS5QNZE8";
+    public const string BabaGhanoush = "01HQ6BZJXP9MAWB3DQXS5QNZE9";
+    public const string LentilSoup = "01HQ6BZJXPAMAWB3DQXS5QNZF1";
+    public const string SayyadiaRice = "01HQ6BZJXPBMAWB3DQXS5QNZF2";
+    public const string AlexandriaPasta = "01HQ6BZJXPCMAWB3DQXS5QNZF3";
+    public const string EgyptianTea = "01HQ6BZJXPDMAWB3DQXS5QNZF4";
+    public const string SugarCaneJuice = "01HQ6BZJXPEMAWB3DQXS5QNZF5";
+    public const string GrilledChicken = "01HQ6BZJXPFMAWB3DQXS5QNZF6";
+    public const string KoftaMeat = "01HQ6BZJXPGMAWB3DQXS5QNZF7";
+    public const string MixedPickles = "01HQ6BZJXPHMAWB3DQXS5QNZF8";
+    public const string EgyptianSpiceMix = "01HQ6BZJXPIMAWB3DQXS5QNZF9";
+    public const string TahiniSauce = "01HQ6BZJXPJMAWB3DQXS5QNZG1";
+    public const string Hawawshi = "01HQ6BZJXPKMAWB3DQXS5QNZG2";
+}
+
+public static class ProductImageIds
+{
+    public const string KosharyMain = "01HQ7BZJXP1MAWB3DQXS5QNZ01";
+    public const string KosharyAlt = "01HQ7BZJXP2MAWB3DQXS5QNZ02";
+    public const string MolokhiaMain = "01HQ7BZJXP3MAWB3DQXS5QNZ03";
+    public const string MolokhiaAlt = "01HQ7BZJXP4MAWB3DQXS5QNZ04";
+    public const string FulMain = "01HQ7BZJXP5MAWB3DQXS5QNZ05";
+    public const string FulAlt = "01HQ7BZJXP6MAWB3DQXS5QNZ06";
+    public const string MixedGrillMain = "01HQ7BZJXP7MAWB3DQXS5QNZ07";
+    public const string MixedGrillAlt = "01HQ7BZJXP8MAWB3DQXS5QNZ08";
+    public const string KunafaMain = "01HQ7BZJXP9MAWB3DQXS5QNZ09";
+    public const string KunafaAlt = "01HQ7BZJXPAMAWB3DQXS5QNZ10";
+    public const string BaladiMain = "01HQ7BZJXPBMAWB3DQXS5QNZ11";
+    public const string BaladiAlt = "01HQ7BZJXPCMAWB3DQXS5QNZ12";
+    public const string SeabassMain = "01HQ7BZJXPDMAWB3DQXS5QNZ13";
+    public const string SeabassAlt = "01HQ7BZJXPEMAWB3DQXS5QNZ14";
+    public const string FattahMain = "01HQ7BZJXPFMAWB3DQXS5QNZ15";
+    public const string FattahAlt = "01HQ7BZJXPGMAWB3DQXS5QNZ16";
+    public const string BabaGhanoushMain = "01HQ7BZJXPHMAWB3DQXS5QNZ17";
+    public const string BabaGhanoushAlt = "01HQ7BZJXPIMAWB3DQXS5QNZ18";
+    public const string LentilSoupMain = "01HQ7BZJXPJMAWB3DQXS5QNZ19";
+    public const string LentilSoupAlt = "01HQ7BZJXPKMAWB3DQXS5QNZ20";
+    public const string SayyadiaMain = "01HQ7BZJXPLMAWB3DQXS5QNZ21";
+    public const string SayyadiaAlt = "01HQ7BZJXPMMAWB3DQXS5QNZ22";
+    public const string AlexPastaMain = "01HQ7BZJXPNMAWB3DQXS5QNZ23";
+    public const string AlexPastaAlt = "01HQ7BZJXPOMAWB3DQXS5QNZ24";
+    public const string EgyptianTeaMain = "01HQ7BZJXPPMAWB3DQXS5QNZ25";
+    public const string EgyptianTeaAlt = "01HQ7BZJXPQMAWB3DQXS5QNZ26";
+    public const string SugarCaneMain = "01HQ7BZJXPRMAWB3DQXS5QNZ27";
+    public const string SugarCaneAlt = "01HQ7BZJXPSMAWB3DQXS5QNZ28";
+    public const string GrilledChickenMain = "01HQ7BZJXPTMAWB3DQXS5QNZ29";
+    public const string GrilledChickenAlt = "01HQ7BZJXPUMAWB3DQXS5QNZ30";
+    public const string KoftaMeatMain = "01HQ7BZJXPVMAWB3DQXS5QNZ31";
+    public const string KoftaMeatAlt = "01HQ7BZJXPWMAWB3DQXS5QNZ32";
+    public const string MixedPicklesMain = "01HQ7BZJXPXMAWB3DQXS5QNZ33";
+    public const string MixedPicklesAlt = "01HQ7BZJXPYMAWB3DQXS5QNZ34";
+    public const string SpiceMixMain = "01HQ7BZJXPZMAWB3DQXS5QNZ35";
+    public const string SpiceMixAlt = "01HQ7BZJXP0MAWB3DQXS5QNZ36";
+    public const string TahiniMain = "01HQ7BZJXP1NAWB3DQXS5QNZ37";
+    public const string TahiniAlt = "01HQ7BZJXP2NAWB3DQXS5QNZ38";
+    public const string HawawshiMain = "01HQ7BZJXP3NAWB3DQXS5QNZ39";
+    public const string HawawshiAlt = "01HQ7BZJXP4NAWB3DQXS5QNZ40";
+}
+
+public static class ProductVariantIds
+{
+    // كشري
+    public const string KosharySmall = "01HQ8BZJXP1MAWB3DQXS5QNZ01";
+    public const string KosharyMedium = "01HQ8BZJXP2MAWB3DQXS5QNZ02";
+    public const string KosharyLarge = "01HQ8BZJXP3MAWB3DQXS5QNZ03";
+
+    // ملوخية
+    public const string MolokhiaChickenSmall = "01HQ8BZJXP4MAWB3DQXS5QNZ04";
+    public const string MolokhiaChickenMedium = "01HQ8BZJXP5MAWB3DQXS5QNZ05";
+    public const string MolokhiaChickenLarge = "01HQ8BZJXP6MAWB3DQXS5QNZ06";
+
+    // فول مدمس
+    public const string FulSandwich = "01HQ8BZJXP7MAWB3DQXS5QNZ07";
+    public const string FulPlate = "01HQ8BZJXP8MAWB3DQXS5QNZ08";
+
+    // مشويات مشكلة
+    public const string MixedGrillSmall = "01HQ8BZJXP9MAWB3DQXS5QNZ09";
+    public const string MixedGrillMedium = "01HQ8BZJXPAMAWB3DQXS5QNZ10";
+    public const string MixedGrillLarge = "01HQ8BZJXPBMAWB3DQXS5QNZ11";
+
+    // كنافة
+    public const string KunafaSlice = "01HQ8BZJXPCMAWB3DQXS5QNZ12";
+    public const string KunafaBox = "01HQ8BZJXPDMAWB3DQXS5QNZ13";
+
+    // سمك قاروص
+    public const string SeabassSmall = "01HQ8BZJXPEMAWB3DQXS5QNZ14";
+    public const string SeabassMedium = "01HQ8BZJXPFMAWB3DQXS5QNZ15";
+    public const string SeabassLarge = "01HQ8BZJXPGMAWB3DQXS5QNZ16";
+
+    // فراخ مشوية
+    public const string ChickenQuarterLight = "01HQ8BZJXPHMAWB3DQXS5QNZ17";
+    public const string ChickenQuarterDark = "01HQ8BZJXPIMAWB3DQXS5QNZ18";
+    public const string ChickenWhole = "01HQ8BZJXPJMAWB3DQXS5QNZ19";
+
+    // كفتة
+    public const string KoftaPlate = "01HQ8BZJXPKMAWB3DQXS5QNZ20";
+    public const string KoftaSandwich = "01HQ8BZJXPLMAWB3DQXS5QNZ21";
+
+    // حواوشي
+    public const string HawawshiSmall = "01HQ8BZJXPMMAWB3DQXS5QNZ22";
+    public const string HawawshiLarge = "01HQ8BZJXPNMAWB3DQXS5QNZ23";
+}
